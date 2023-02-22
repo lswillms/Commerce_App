467bbaa40f7a98d56d1bedf1ff9c9598dad76da6
Latoshia Williams <lswillms@yahoo.com>
2023-02-22 13:55:35 -0600

    updated navbar

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -1,18 +1,45 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import ProductList from "./ProductList"
-import ProductPage from "./ProductPage"
-
+import {Route, Routes} from "react-router-dom"
+import 'semantic-ui-css/semantic.min.css'
+import NewProductForm from "./NewProductForm"
+import NavBar from "./NavBar"
+import ProductDetails from "./ProductDetails"
 
 
 function App() {
+ 
+   const [ products, setProducts] = useState([])
 
-  
+   useEffect(() => {
+       fetch("http://localhost:9292/products")
+       .then((resp)=> resp.json())
+       .then((data) => setProducts(data))
+   }, [])
+
+   const addProductsToPage= products.map((product) => (
+      <ProductDetails
+      key = {product.id}
+      product = {product}
+  />
+      ))
+
+   function handleAddNewProduct(newProduct) {
+         setProducts([...products, newProduct])
+     }
 
 
 return (
 <div className="app">
-   <ProductList/>
-   <ProductPage/>
+<div>
+<NavBar/>
+      <Routes>
+      <Route  path = "/productlist" element={<ProductList addProductsToPage = {addProductsToPage}/>}>
+      </Route>
+        <Route  path = "/newproductform" element={<NewProductForm handleAddNewProduct = {handleAddNewProduct}/>}>
+       </Route>
+      </Routes>
+      </div>
 </div>
 )
 }
--- src/componenets/ProductPage.js
+++ src/componenets/ProductPage.js
@@ -1,33 +1,17 @@
 import React,{useState, useEffect} from "react";
-import ProductList from "./ProductList";
-import ProductDetail from "./ProductDetails";
-import NewProductForm from "./NewProductForm";
-
+i
 function ProductPage() {
 
-const [ products, setProducts] = useState([])
 
-useEffect(() => {
-    fetch("http://localhost:9292/products")
-    .then((resp)=> resp.json())
-    .then((data) => setProducts(data))
-}, [])
 
-const addProductsToPage= products.map((product) => (
-    <ProductDetail
-    key = {product.id}
-    product = {product}
-/>
-    ))
 
-function handleAddNewProduct(newProduct) {
-    setProducts([...products, newProduct])
-}
+
+
 
 return (
     <main>
         <ProductList addProductsToPage = {addProductsToPage}/>
-        <NewProductForm handleAddNewProduct = {handleAddNewProduct}/>
+       
     </main>
 )
 }
